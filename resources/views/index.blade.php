bf9a7d5480727250ed4b04cfeb4f5b59ef580a5b
Gao <sgao@gdb-hollywood.gavindebecker.com>
2019-06-25 02:11:56 -0700

    added buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
 				<a class="nav-link active" href="#language_skills" role="tab" data-toggle="tab">Language Skills</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#experienced_countries" role="tab" data-toggle="tab">Experienced Countries</a>
+				<a class="nav-link" href="#experienced_countries" role="tab" data-toggle="tab">Countries Experienced</a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="#hobbies" role="tab" data-toggle="tab">Hobbies</a>
@@ -29,9 +29,15 @@
 		</ul>
 		<div class="tab-content card-body">
 			<div class="tab-pane fade show active" id="language_skills" role="tabpanel" aria-labelledby="home-tab">
+				<div class="d-flex justify-content-center">
+					<a href={{url('/userLanguageSkills/create')}} class="btn btn-success mb-3">Add New Language Skill</a>
+				</div>
 				@include('includes.userLanguageSkillList')
 			</div>
 			<div class="tab-pane fade" id="experienced_countries" role="tabpanel" aria-labelledby="profile-tab">
+				<div class="d-flex justify-content-center">
+					<a href={{url('/userCountryExperience/create')}} class="btn btn-success mb-3">Add New Country Experience</a>
+				</div>
 				@include('includes.userExperiencedCountryList')
 			</div>
 			<div class="tab-pane fade" id="hobbies" role="tabpanel" aria-labelledby="messages-tab">
